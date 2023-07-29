5d1bd673337acffdfa9230fbab48436e4f733526
suhanigupta03 <111136014+suhanigupta03@users.noreply.github.com>
2023-07-29 18:53:16 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+
+import './App.css'
+import Hostel from './component/Hostel'
+import Class from './component/Classr'
+import Gym from './component/Gym'
+import Herbal from './component/Herbalgarden'
+import Library from './component/Library'
+function App() {
+    return (
+      <div>
+    
+      <Hostel /> 
+      <Class/> 
+      <Gym/> 
+      <Herbal/>
+      <Library/>
+      </div>
+    );
+  }
+  
+  export default App;
